e9b0d6baccbbdf5ba5355f2bb165e90656e04860
Leon Chan <leonchantinglok@gmail.com>
2025-04-03 11:08:47 +0800

    feat: set up boost DI example as starting point

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,17 @@
 #include "logger/ilogger.h"
 #include "logger/console_logger.h"
 #include "app.h"
+#include <boost/di.hpp>
+
+namespace di = boost::di;
 
 int main()
 {
-    ConsoleLogger logger;
-    App app(logger);
+    auto injector = di::make_injector(
+        di::bind<ILogger>().to<ConsoleLogger>().in(di::singleton)); // singleton
+    // di::bind<ILogger>().to<ConsoleLogger>()); // by default it is transient
+    // di::bind<ILogger>().to<ConsoleLogger>().in(di::unique)); // to state it explicitly as transient
+    auto app = injector.create<App>();
     app.run();
     return 0;
 }
