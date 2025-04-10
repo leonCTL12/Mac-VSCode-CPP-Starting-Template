7883207833ade1452c1c541cff64d3db138796e8
Leon Chan <leonchantinglok@gmail.com>
2025-04-10 16:10:58 +0800

    Revert "feat: separating CMakeList into test and main"
    This reverts commit 80cb5ae8d1d517eada70315afe46c7bb4f290acf.

--- tests/hello_world_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <iostream>
-// The simplest test ever
-TEST(HelloWorld, SaysHello)
-{
-    std::cout << "Hello, World Test!" << std::endl;
-    EXPECT_EQ(1 + 1, 2); // Basic arithmetic check
-}
