98e0e59a02b2e5ec7e5c014ef43651e2f5be43cb
Leon Chan <leonchantinglok@gmail.com>
2025-04-10 08:46:32 +0000

    feat: create a new task in task.json to build and run unit tests

--- tests/calculator_test.cpp
+++ tests/calculator_test.cpp
@@ -8,3 +8,11 @@ TEST(CalculatorTest, Addition)
     EXPECT_EQ(calc.add(-1, 1), 0);
     EXPECT_EQ(calc.add(0, 0), 0);
 }
+
+TEST(CalculatorTest, AdditionAgain)
+{
+    Calculator calc;
+    EXPECT_EQ(calc.add(2, 3), 5);
+    EXPECT_EQ(calc.add(-1, 1), 0);
+    EXPECT_EQ(calc.add(0, 0), 0);
+}
